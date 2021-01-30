0cf10a6c75ddffa64dd593deb8087e6ce7843f11
Anthony Merlier <webmaster@consor.fr>
2021-01-30 19:20:51 +0100

    Premiers pages créées

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController {
+
+    public function index(){
+        echo "Hello World !";
+        die;
+    }
+
+    /**
+     * @Route("/about/{age<\d+>?0}", name="test", methods={"GET", "POST"}, host="localhost", schemes={"http", "https"})
+     */
+    public function about(Request $request, $age){
+        return new Response("Vous avez $age ans !");
+    }
+
+}
